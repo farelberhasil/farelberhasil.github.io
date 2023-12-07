dbe639abff90d323d4a166c7b37afebfb9985112
farelberhasil <153213729+farelberhasil@users.noreply.github.com>
2023-12-07 08:27:44 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
-const awaljpg = document.querySelector(".awal.jpg");
+const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
